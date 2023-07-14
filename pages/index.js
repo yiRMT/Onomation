7e5114564b0b7425c57e236fee0481eb1c9ba3d0
naoyalab <sdb01072@st.osakafu-u.ac.jp>
2023-07-14 22:36:42 +0900

    conflict解消

--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,6 @@ import Image from 'next/image'
 import { ChakraProvider } from '@chakra-ui/react'
 import Home_onomation from '@/Components/home'
 import bgp from '../assets/images/bgp.png'
-<<<<<<< Updated upstream
-
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
-  return (
-    <ChakraProvider>
-    <main className='bg-gray-100' >
-      <section className="flex min-h-screen flex-col items-center justify-center p-24" style={{backgroundImage: `url(${bgp})`} }>
-      
-      <Home_onomation/>
-=======
 import Onogen from '@/Components/Card/Onogen'
 import Onologin from '@/Components/Card/Onologin'
 
@@ -24,17 +12,11 @@ export default function Home() {
   useEffect(() => {
     const container = containerRef.current;
     let position = -300;
->>>>>>> Stashed changes
 
     const scrollBackground = () => {
       position += 1;
       container.style.top = `${position}px`;
 
-<<<<<<< Updated upstream
-
-      </section>
-      
-=======
       if (position >= 0) {
         position = -300;
         container.style.transition = '0.1s ease-out';
@@ -45,22 +27,9 @@ export default function Home() {
 
       requestAnimationFrame(scrollBackground);
     };
->>>>>>> Stashed changes
 
     scrollBackground();
 
-<<<<<<< Updated upstream
-      <section className="flex flex-col items-center justify-center p-24">
-        <Form/>
-
-
-      </section>
-
-    
-      
-      
-    </main>
-=======
     return () => cancelAnimationFrame(scrollBackground);
   }, []);
 
@@ -85,7 +54,6 @@ export default function Home() {
           </section>
         </div>
       </main>
->>>>>>> Stashed changes
     </ChakraProvider>
   )
 }
