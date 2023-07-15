68cfe4e6765e6e3eb4fd44da8259433335424a37
Yuichiro Iwashita <9864901+yiRMT@users.noreply.github.com>
2023-07-15 12:55:31 +0900

    認証状態の管理を実装

--- Components/Card/OnoSignIn.js
+++ Components/Card/OnoSignIn.js
@@ -1,30 +1,42 @@
 import WorksCard from "./Card";
-import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { signInWithPopup, signOut, GoogleAuthProvider } from "firebase/auth";
 import { auth } from "@/firebase";
+import { useContext, useEffect } from "react";
+import AuthContext from "@/libs/context/AuthContext";
 
 const OnoSignIn = () => {
+  const {authState, authDispatch} = useContext(AuthContext);
+
   const provider = new GoogleAuthProvider();
 
   const handleSignIn = async () => {
-    try {
-      const result = await signInWithPopup(auth, provider);
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-      // The signed-in user info.
-      const user = result.user;
-      // IdP data available using getAdditionalUserInfo(result)
-      // ...  
-      console.log(user);
-    } catch (error) {
-      console.log(error);
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.customData.email;
-      // The AuthCredential type that was used.
-      const credential = GoogleAuthProvider.credentialFromError(error);
-      // ...
+    if (authState.user) {
+      try {
+        await signOut(auth);
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      try {
+        const result = await signInWithPopup(auth, provider);
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...  
+        console.log(user);
+      } catch (error) {
+        console.log(error);
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      }
     }
   }
 
@@ -57,10 +69,10 @@ const OnoSignIn = () => {
             "pointer-events-none",
           ].join(" ")}
         >
-          Googleアカウントでログインする
+          { authState.user ? "ログアウトする" : "Googleアカウントでログインする" }
         </span>
-        <button onClick={handleSignIn}>
-          <WorksCard title={"ログイン"} />
+        <button onClick={ handleSignIn }>
+          <WorksCard title={ authState.user ? "ログアウト" : "ログイン" } />
         </button>
       </span>
     </>
