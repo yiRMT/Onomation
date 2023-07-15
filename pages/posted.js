541b4dcd812a45643502d61c111641cafb5e1358
Yuichiro Iwashita <9864901+yiRMT@users.noreply.github.com>
2023-07-15 16:35:52 +0900

    投稿一覧ページのコードを修正

--- pages/posted.js
+++ pages/posted.js
@@ -6,32 +6,25 @@ import HomeLogo from '@/Components/home_logo'
 import Form from '@/Components/form'
 import Cssoutput from '@/Components/cssoutput'
 import Onogen from '@/Components/Card/Onogen'
-import Posted_comp from '@/Components/Post/posted_component';
+import Post from '@/Components/Post';
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Posted() {
   const sample = {
-    uid:"1102",
-    postDate:"2023-7-15",
-    original_text:"ザーザー",
-    comment:"かわいい",
+    uid: "1102",
+    displayName: "マサキング",
+    postDate: "2023-7-15",
+    original_text: "ザーザー",
+    comment: "かわいい",
     animation: {
-      html:'<!DOCTYPE html><html><head><title>ザーザー Animation</title><link rel="stylesheet" type="text/css"></head><body><div id="container"><div id="circle"></div></div></body></html>',
-      css:'#container { position: relative; width: 200px; height: 200px; overflow: hidden; } #circle { position: absolute; width: 100%; height: 100%; border-radius: 50%; background-color: blue; } ',
-      javascript:'const circle = document.getElementById("circle"); function animateCircle() { circle.style.transform = "scale(2)"; circle.style.transition = "transform 0.5s ease-in-out"; setTimeout(function() { circle.style.transform = "scale(1)"; circle.style.transition = "transform 0.5s ease-in-out"; }, 500); setTimeout(animateCircle, 1000); } animateCircle(); ',
-
-
+      html: '<!DOCTYPE html><html><head><title>ザーザー Animation</title></head><body><div id="container"><div id="circle"></div></div></body></html>',
+      css: '#container { position: relative; width: 200px; height: 200px; overflow: hidden; } #circle { position: absolute; width: 100%; height: 100%; border-radius: 50%; background-color: blue; } ',
+      javascript: 'const circle = document.getElementById("circle"); function animateCircle() { circle.style.transform = "scale(2)"; circle.style.transition = "transform 0.5s ease-in-out"; setTimeout(function() { circle.style.transform = "scale(1)"; circle.style.transition = "transform 0.5s ease-in-out"; }, 500); setTimeout(animateCircle, 1000); } animateCircle(); ',
     }
-
-
-
-
-
-
-
-
-
   }
+
+  const postsSample = [ sample, sample ]
+
   const containerRef = useRef(null);
 
   useEffect(() => {
@@ -57,8 +50,9 @@ export default function Posted() {
 
     return () => cancelAnimationFrame(scrollBackground);
   }, []);
+
   return (
-    <main className='flex flex-col items-center justify-center' >
+    <main className='flex' >
       <div ref={containerRef} className='-z-50 background-container'>
         <Image
           src = "/bgp2.png"
@@ -67,9 +61,15 @@ export default function Posted() {
           objectFit='cover'
         />
       </div>
-      <section className="flex min-h-screen flex-col items-center justify-center" >
-        <Posted_comp posts={sample}/>
-      </section>
+      <div className="mx-96 my-10 w-full" >
+        <ul className='flex flex-col gap-5'>
+          {postsSample.map((post) => (
+            <li key={post.postDate}>
+              <Post post={ post } />
+            </li>
+          ))}
+        </ul>
+      </div>
     </main>
   )
 }
